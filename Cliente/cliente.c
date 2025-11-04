267e0712d3a1dde1be0d8c2b241f2f9cf8a146ae
Matías N. López <matias@matnalopez.com.ar>
2025-11-03 21:20:10 -0300

    Borramos el hardcodeo del nick

--- Cliente/cliente.c
+++ Cliente/cliente.c
@@ -171,11 +171,11 @@ int main(int argc, char* argv[]) {
     int port = atoi(argv[2]);
     int sockfd;
     char buffer[BUF_SIZE] = {0};
-    char nick[32] = "Ignacio";
+    char nick[32];
     
     printf("\n=== CLIENTE DE CHAT ===\n");
     
-    /*
+    
     // Solicitar nick
     printf("Ingresa tu nick: ");
     fflush(stdout);
@@ -192,7 +192,7 @@ int main(int argc, char* argv[]) {
         printf("El nick no puede estar vacío\n");
         return EXIT_FAILURE;
     }
-    */
+    
 
     printf("Conectando a %s:%d...\n", ip, port);
 
